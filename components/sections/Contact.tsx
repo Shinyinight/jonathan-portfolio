e2f6136ac9ed76c053667349963ee43a8f401bb9
jonathanfernandezfm <jonathanfernandezfm@gmail.com>
2023-04-20 23:13:10 +0300

    Refactoring of components

--- components/sections/Contact.tsx
+++ components/sections/Contact.tsx
@@ -1,39 +1,41 @@
-import { MutableRefObject } from 'react';
-import { Envelope } from 'phosphor-react';
+import { MutableRefObject } from "react";
+import { Envelope } from "phosphor-react";
 
-import ContactForm from '@components/ContactForm';
-import contact_links from '@data/contact';
+import ContactForm from "@/components/ContactForm";
+import contact_links from "@/data/contact";
 
 interface ContactProps {
-	contactRef: MutableRefObject<any>;
-	contactFormRef: MutableRefObject<any>;
+    contactRef: MutableRefObject<any>;
+    contactFormRef: MutableRefObject<any>;
 }
 
 const Contact = ({ contactRef, contactFormRef }: ContactProps) => {
-	return (
-		<section
-			ref={contactRef}
-			className="flex flex-col justify-center min-h-screen py-16 text-center xl:pb-40 contact"
-		>
-			<h1 className="text-4xl font-bold xl:text-5xl font-rubik">Contact</h1>
-			<div className="flex flex-wrap justify-center gap-4 px-4 mt-16 xl:px-0 xl:gap-16 contact-links">
-				{contact_links.map((link) => {
-					return (
-						<a
-							key={link.name}
-							data-tip={link.name}
-							href={link.url}
-							target={link.blank ? '_blank' : ''}
-							className="p-2 rounded link-contact focus:outline-none focus:ring-4 focus:ring-white focus:border-transparent"
-						>
-							{link.icon}
-						</a>
-					);
-				})}
-			</div>
-			<ContactForm contactFormRef={contactFormRef} />
-		</section>
-	);
+    return (
+        <section
+            ref={contactRef}
+            className="flex flex-col justify-center min-h-screen py-16 text-center xl:pb-40 contact"
+        >
+            <h1 className="text-4xl font-bold xl:text-5xl font-rubik">
+                Contact
+            </h1>
+            <div className="flex flex-wrap justify-center gap-4 px-4 mt-16 xl:px-0 xl:gap-16 contact-links">
+                {contact_links.map((link) => {
+                    return (
+                        <a
+                            key={link.name}
+                            data-tip={link.name}
+                            href={link.url}
+                            target={link.blank ? "_blank" : ""}
+                            className="p-2 rounded link-contact focus:outline-none focus:ring-4 focus:ring-white focus:border-transparent"
+                        >
+                            {link.icon}
+                        </a>
+                    );
+                })}
+            </div>
+            <ContactForm contactFormRef={contactFormRef} />
+        </section>
+    );
 };
 
 export default Contact;
--- components/sections/Home.tsx
+++ components/sections/Home.tsx
@@ -1,65 +1,72 @@
-import Image from 'next/image';
-import { MutableRefObject } from 'react';
+import Image from "next/image";
+import { MutableRefObject } from "react";
 
 interface HomeProps {
-	projectsRef: MutableRefObject<any>;
-	contactRef: MutableRefObject<any>;
+    projectsRef: MutableRefObject<any>;
+    contactRef: MutableRefObject<any>;
 }
 
 const Home = ({ projectsRef, contactRef }: HomeProps) => {
-	const scrollTo = (ref: MutableRefObject<any>) => ref.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    const scrollTo = (ref: MutableRefObject<any>) =>
+        ref.current.scrollIntoView({ behavior: "smooth", block: "start" });
 
-	return (
-		<section
-			id="home"
-			className="container flex flex-col items-center justify-end min-h-screen px-6 pt-24 pb-10 m-auto bg-no-repeat xl:h-screen xl:py-0 mt-22 xl:flex-row main-hero"
-		>
-			<div className="relative flex items-center justify-center flex-1 h-full mt-8 overflow-hidden xl:flex-1 2xl:w-96 2xl:h-96 md:w-4/5">
-				<div className="relative w-56 h-56 bg-white border-8 border-white rounded-full shadow-lg xl:w-96 xl:h-96">
-					<Image
-						className="overflow-hidden rounded-full shadow-lg grayscale-0"
-						alt="Jonathan Photo"
-						width={400}
-						height={400}
-						src="/images/jonathan_photo.png"
-					/>
-				</div>
-			</div>
-			<div className="flex-1 xl:flex-1 ">
-				<div className="absolute left-0 z-0 w-full top-10 xl:w-14 xl:left-16">
-					<div className="relative mx-auto w-14 h-14">
-						<Image layout="fill" objectFit="contain" src="/images/logo.svg" alt="logo portfolio" />
-					</div>
-				</div>
-				<h1 className="mt-8 text-4xl text-center md:text-6xl xl:mt-0 xl:text-7xl xl:text-left font-rubik">
-					Hi, I'm <b>Jonathan</b>
-				</h1>
-				<p className="px-12 mt-6 text-xl leading-snug text-center md:text-2xl xl:text-left xl:px-0 text-1xl xl:text-3xl 2xl:text-4xl">
-					I'm an Information Systems Engineer, Full Stack Developer and web enthusiast from Spain.
-					<br />
-					I'm currently living in Kuopio, Finland.
-				</p>
-				<div className="flex justify-center gap-8 xl:justify-start my-14 xl:my-0 xl:mt-12 buttons">
-					<button
-						onClick={() => {
-							scrollTo(projectsRef);
-						}}
-						className="px-10 py-2 text-xl font-bold tracking-wide rounded shadow-xl md:px-12 md:py-3 xl:py-3 xl:text-xl xl:px-14 button-primary focus:outline-none focus:ring-4 focus:ring-purple-400 focus:border-transparent"
-					>
-						Projects
-					</button>
-					<button
-						onClick={() => {
-							scrollTo(contactRef);
-						}}
-						className="px-10 py-2 text-xl font-bold tracking-wide rounded shadow-xl xl:py-3 xl:text-xl xl:px-14 button-primary focus:outline-none focus:ring-4 focus:ring-white focus:border-transparent"
-					>
-						Contact
-					</button>
-				</div>
-			</div>
-		</section>
-	);
+    return (
+        <section
+            id="home"
+            className="container flex flex-col items-center justify-end min-h-screen px-6 pt-24 pb-10 m-auto bg-no-repeat xl:h-screen xl:py-0 mt-22 xl:flex-row main-hero"
+        >
+            <div className="relative flex items-center justify-center flex-1 h-full mt-8 overflow-hidden xl:flex-1 2xl:w-96 2xl:h-96 md:w-4/5">
+                <div className="relative w-56 h-56 bg-white border-8 border-white rounded-full shadow-lg xl:w-96 xl:h-96">
+                    <Image
+                        className="overflow-hidden rounded-full shadow-lg grayscale-0"
+                        alt="Jonathan Photo"
+                        width={400}
+                        height={400}
+                        src="/images/jonathan_photo.png"
+                    />
+                </div>
+            </div>
+            <div className="flex-1 xl:flex-1 ">
+                <div className="absolute left-0 z-0 w-full top-10 xl:w-14 xl:left-16">
+                    <div className="relative mx-auto w-14 h-14">
+                        <Image
+                            layout="fill"
+                            objectFit="contain"
+                            src="/images/logo.svg"
+                            alt="logo portfolio"
+                        />
+                    </div>
+                </div>
+                <h1 className="mt-8 text-4xl text-center md:text-6xl xl:mt-0 xl:text-7xl xl:text-left font-rubik">
+                    Hi, I'm <b>Jonathan</b>
+                </h1>
+                <p className="px-12 mt-6 text-xl leading-snug text-center md:text-2xl xl:text-left xl:px-0 text-1xl xl:text-3xl 2xl:text-4xl">
+                    I'm an Information Systems Engineer, Full Stack Developer
+                    and web enthusiast from Spain.
+                    <br />
+                    I'm currently living in Kuopio, Finland.
+                </p>
+                <div className="flex justify-center gap-8 xl:justify-start my-14 xl:my-0 xl:mt-12 buttons">
+                    <button
+                        onClick={() => {
+                            scrollTo(projectsRef);
+                        }}
+                        className="px-10 py-2 text-xl font-bold tracking-wide rounded shadow-xl md:px-12 md:py-3 xl:py-3 xl:text-xl xl:px-14 button-primary focus:outline-none focus:ring-4 focus:ring-purple-400 focus:border-transparent"
+                    >
+                        Projects
+                    </button>
+                    <button
+                        onClick={() => {
+                            scrollTo(contactRef);
+                        }}
+                        className="px-10 py-2 text-xl font-bold tracking-wide rounded shadow-xl xl:py-3 xl:text-xl xl:px-14 button-primary focus:outline-none focus:ring-4 focus:ring-white focus:border-transparent"
+                    >
+                        Contact
+                    </button>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Home;
